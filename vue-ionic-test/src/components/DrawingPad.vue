52fbdc00a3f479041939533b6ecd309d2b4eedb7
martin gardner II <martingardnerii@gmail.com>
2020-01-09 12:16:12 -0600

    DrawingPad.vue added in demo of clear, save, and load buttons for canvas bitmap

--- vue-ionic-test/src/components/DrawingPad.vue
+++ vue-ionic-test/src/components/DrawingPad.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="drawing-page">
     <canvas width="400" height="200"></canvas>
+    <ion-item>
+      <ion-button @click="clearPad">Clear</ion-button>
+      <ion-button @click="saveDataToSVG">Save Image</ion-button>
+      <ion-button @click="loadCanvasWithSVG">Load Image</ion-button>
+    </ion-item>
   </div>
 </template>
 
@@ -10,21 +15,25 @@ import SignaturePad from "signature_pad";
 export default {
   name: "DrawingPad",
   data() {
-    return {};
+    return {
+      signaturePad: null
+    };
   },
   methods: {
     signaturePadSetup() {
       var canvas = document.querySelector("canvas");
 
-      var signaturePad = new SignaturePad(canvas);
-      console.log("signaturePad", signaturePad);
+      this.signaturePad = new SignaturePad(canvas);
+
       // Returns signature image as data URL (see https://mdn.io/todataurl for the list of possible parameters)
       //signaturePad.toDataURL(); // save image as PNG
       //signaturePad.toDataURL("image/jpeg"); // save image as JPEG
       //signaturePad.toDataURL("image/svg+xml"); // save image as SVG
 
       // Draws signature image from data URL.
-      // NOTE: This method does not populate internal data structure that represents drawn signature. Thus, after using #fromDataURL, #toData won't work properly.
+      // NOTE: This method does not populate internal data
+      //structure that represents drawn signature.
+      //Thus, after using #fromDataURL, #toData won't work properly.
       //signaturePad.fromDataURL("data:image/png;base64,iVBORw0K...");
 
       // Returns signature image as an array of point groups
@@ -44,6 +53,36 @@ export default {
 
       // Rebinds all event handlers
       //signaturePad.on();
+    },
+    clearPad() {
+      //clear canvas
+      this.signaturePad.clear();
+    },
+    saveDataToSVG() {
+      //let imageData = this.signaturePad.toDataURL("image/svg+xml");
+      let imageData = this.signaturePad.toDataURL();
+      console.log("imageData", imageData);
+      localStorage.setItem("image1", imageData);
+    },
+    loadCanvasWithSVG() {
+      //this.signaturePad.fromDataURL("data:image/png;base64,iVBORw0K...");
+      let canvas = document.querySelector("canvas");
+      let ctx = canvas.getContext("2d");
+      let imageData = localStorage.getItem("image1");
+      let img = new Image();
+
+      img.src = imageData;
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0);
+      };
+
+      /*
+      img.onload = () => {
+        document.querySelector("canvas").drawImage(img, 0, 0);
+      };
+      */
+      //this.signaturePad.fromDataURL(`data:image/png;base64,${imageData}`);
+      //this.signaturePad.fromDataURL("data:image/png;base64,iVBORw0K...");
     }
   },
   mounted() {
@@ -57,3 +96,9 @@ canvas {
   border: 1px solid black;
 }
 </style>
+
+<!--
+https://www.npmjs.com/package/signature_pad
+https://github.com/szimek/signature_pad
+
+-->
